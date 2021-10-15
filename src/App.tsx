31dd6ca833e473e6cb7273a019f84232463ccb25
Atharva Bhange <atharva.bhange@gmail.com>
2021-10-15 11:10:29 +0530

    Bug : dark mode not working on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "Navigation";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
 	const queryClient = new QueryClient();
+
+	useEffect(() => {
+		const body = document.getElementsByTagName("body")[0];
+		const localDarkMode = localStorage.getItem("darkMode");
+		if (localDarkMode === "true") body.classList.toggle("dark", true);
+		else if (localDarkMode === "false") body.classList.toggle("dark", false);
+		else if (
+			window.matchMedia &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches
+		) {
+			body.classList.toggle("dark", true);
+			localStorage.setItem("darkMode", "true");
+		}
+	}, []);
+
 	return (
 		<>
 			<QueryClientProvider client={queryClient}>
